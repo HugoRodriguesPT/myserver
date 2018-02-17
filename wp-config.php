15666d64ca250f96535add6f63814f17bcfb8844
Hugo Rodrigues <mail.hugo.r@gmail.com>
2018-02-17 05:28:21 +0000

    config wp

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'wordpressadmin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'adminpassword');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -46,6 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
+define('AUTH_KEY',         '=5gk5Uh.!QhN-<G{]{KdU;5i},1+-+I[Z.+eNQ Wx>v.ZS#Hkk;/$3S{d~jE_|-G');
+define('SECURE_AUTH_KEY',  'f)$d3~O|gJA7n$(*C@FQ;KBQ{X;,JI[YRI`f-kn+]N@R|f/k9v/%Ie+U, tcu(1I');
+define('LOGGED_IN_KEY',    '0tVa{4V1/R77I&]/r${~a[b6Y]KeZnak|.a{oW(F&)Dm:W%8/|^ToE#aFug:juM^');
+define('NONCE_KEY',        '<r>r:eB{06PHFHH<9xRMVTI.`:1_h5:bg#n<N~st852170(5m`_ax:>N^N!-i6Vu');
+define('AUTH_SALT',        '#_@,Kr!?hb/&N|A*p[MvtX[Pg7j,NIHz,O!$Fjo6Z)R070Y+JgC`]9T-MH],b:eR');
+define('SECURE_AUTH_SALT', 'O>,##W*#}Y]7IZ ahf2^r)6`t07[k71*=p3#.Jev(:abjd:`S3`gY4ZJC~W-:|;E');
+define('LOGGED_IN_SALT',   'L?Mcs]nG[R~-K-UlE5`tow qV-1+)`CkWBBnCsJqRf_dQGr14=kcN(I:<.VDh6sk');
+define('NONCE_SALT',       '^!|C2bCqqrrC*-{;#nmooO!+l%vL>H,MYQfv9]IW,@`cB`,MQq#b.EF5?^l??bY+');
 
 
 /**#@-*/
@@ -80,4 +88,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
